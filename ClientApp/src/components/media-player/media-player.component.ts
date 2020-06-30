d36fa82c95d63a0506cdbc14108f1f8166b47955
SolvarD <solvar@msn.com>
2020-06-30 08:40:07 +0200

    update size app

--- ClientApp/src/components/media-player/media-player.component.ts
+++ ClientApp/src/components/media-player/media-player.component.ts
@@ -10,38 +10,41 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 })
 export class MediaPlayerComponent implements OnInit {
 
-  @Output()
-  videoStat: Subject<Video> = new Subject<Video>();
   @Input()
   video: Video;
+  @Output()
+  videoStat: Subject<Video> = new Subject<Video>();
+
 
   currentVideo: Video;
   videoElement: HTMLVideoElement;
   statPause: boolean = false;
   statPlay: boolean = true;
-
   safePath: SafeUrl;
   constructor(private ref: ChangeDetectorRef, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.videoElement = document.getElementsByTagName('video')[0];
-    console.log(this.video);
+
     this.safePath = this.sanitizer.bypassSecurityTrustUrl(this.video.path);
+
     this.videoElement.addEventListener('play', (x) => {
       console.log('play', x);
       this.statPause = false;
       this.statPlay = true;
       this.ref.detectChanges();
     });
+
     this.videoElement.addEventListener('pause', (x) => {
       this.statPause = true;
       this.statPlay = false;
       this.ref.detectChanges();
     });
+
     this.videoElement.addEventListener('timeupdate', (x) => {
       this.video.currentTime = this.videoElement.currentTime;
-      this.videoStat.next(this.video);
       this.ref.detectChanges();
+      this.videoStat.next(this.video);
     });
   }
 
@@ -60,6 +63,7 @@ export class MediaPlayerComponent implements OnInit {
   play() {
     this.videoElement.play();
   }
+
   fullScreen() {
     this.videoElement.requestFullscreen();
   }
